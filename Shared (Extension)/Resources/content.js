c4a0c15ef8f0484ff0bd311d87fdec1916ac3616
Pasha Khomchenko <pavel.kh005@gmail.com>
2022-08-03 15:07:10 -0400

    block promoted, messages drawer, trending and who to follow

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,24 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+let adsHidden = 0;
+let runs = 0;
+let adSelector = "div[data-testid=top-impression-pixel]";
+let happeningSelector = "div[aria-label='Trending']";
+let messagesSelector = "div[data-testid=DMDrawer]";
+new MutationObserver(() => {
+  document.querySelectorAll(adSelector).forEach((ad) => {
+    // Twitter detects if you take down the whole tweet, so we are removing its children (:
+    ad.parentElement.parentElement.parentElement.remove();
+    adsHidden++;
+  });
+  const happening = document.querySelector(happeningSelector);
+  if (happening) {
+    happening.remove();
+  }
+  const messages = document.querySelector(messagesSelector);
+  if (messages) {
+    messages.remove();
+  }
+  console.log(`Runs: ${++runs}, Ads hidden: ${adsHidden}`);
+}).observe(document.body, {
+  subtree: true,
+  childList: true,
 });
